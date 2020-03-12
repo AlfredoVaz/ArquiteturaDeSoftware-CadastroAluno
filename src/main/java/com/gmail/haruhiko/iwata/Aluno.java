0303dfa852694b28cd2659745a826ebf2abe236e
Alfredo Vaz <alfredovazn@gmail.com>
2020-03-11 21:06:43 -0300

    Correções de código

---
+++ src/main/java/com/gmail/haruhiko/iwata/Aluno.java
@@ -0,0 +1,42 @@
+package com.gmail.haruhiko.iwata;
+
+// Nome de váriaveis compreensiveis - dava a entender uma lista
+//public class Alunos{
+
+public class Aluno{
+   private String nome;
+   
+//   Uso minimo de recursos - linhas
+//   private String RA;
+//   private String curso;
+
+   public Aluno(){
+     
+   }
+
+   public String getNome(){
+     return nome;
+   }
+
+   public void setNome(String name){
+     this.nome = name;
+   }
+   
+// Uso minimo de recursos - linhas  
+//
+//   public String getRA(){
+//     return RA;
+//   }
+//
+//   public void setRA(String r ){
+//     this.RA = r;
+//   }
+//
+//   public String getCurso(){
+//     return curso;
+//   }
+//
+//   public void setCurso(String cur){
+//     this.curso = cur;
+//   }
+}
--- src/main/java/com/gmail/haruhiko/iwata/App.java
+++ src/main/java/com/gmail/haruhiko/iwata/App.java
@@ -1,25 +1,45 @@
 package com.gmail.haruhiko.iwata;
-import org.apache.commons.codec.digest.DigestUtils;
-import java.util.Scanner;
-import java.util.ArrayList;
-
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App{
 
+    private static ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+	
     public static void main(String[] args) throws IOException {
-      Scanner r = new Scanner(System.in);
+    	
+//    	Nome de váriaveis compreensiveis
+//      Scanner r = new Scanner(System.in);
+      
+      Scanner leitor = new Scanner(System.in);
 
-      Alunos aluno;
+// Nome de váriaveis compreensiveis - dava a entender uma lista
+//  Alunos aluno;
+      
+      Aluno aluno;
+      
+     // Reutilização dos componentes
+     // ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+      
+      
+ // Nome de váriaveis compreensiveis - dava a entender uma lista
+//      ArrayList<Alunos> alunos = new ArrayList<Alunos>();
 
-      ArrayList<Alunos> alunos = new ArrayList<Alunos>();
-
-      int i=0;
-      int x=0;
-      char sn;
+//    int i=0; Uso minimo de recursos
+      
+//      Nome de variaveis comp
+//      int x=0;
+      
+      int opcaoMenu=0;
+      
+//		Nome de Variaveis compreensiveis - quebra de complexidade
+//      char sn;
+      
+      char opcaoContinuar;
+      
       int a=1;
-      int n=0;
+//    int n=0; Uso minimo de recursos
 
       System.out.println("BEM VINDO AO CADASTRO DE ALUNOS");
 
@@ -28,68 +48,104 @@ public class App{
         System.out.println("< 1 > - Cadastrar");
         System.out.println("< 2 > - Consultar");
         System.out.println("< 3 > - Sair");
-        x = r.nextInt();
-        r.nextLine();
-        switch (x){
+        
+//        Nome de váriaveis compreensiveis
+//        x = r.nextInt();
+//        r.nextLine();
+        
+        
+        opcaoMenu = leitor.nextInt();
+        leitor.nextLine();
+        
+        
+        switch (opcaoMenu){
           case 1:
             System.out.println("Informe os dados:");
             do{
 
               System.out.println("Nome: ");
-              String nome = r.nextLine();
+              
+              // Variaveis com nome compreensiveis
+//              String nome = r.nextLine();
+              String nome = leitor.nextLine();
+              
+//				Uso minimo de recursos - linhas
+//              System.out.println("RA: ");
+//              String ra = r.nextLine();
+//
+//              System.out.println("Curso: ");
+//              String curso = r.nextLine();
 
-              System.out.println("RA: ");
-              String ra = r.nextLine();
+              
+       // Nome de váriaveis compreensiveis - dava a entender uma lista
+//              aluno = new Alunos();
 
-              System.out.println("Curso: ");
-              String curso = r.nextLine();
-
-              aluno = new Alunos();
-
+              aluno = new Aluno();
+              
               aluno.setNome(nome);
-              aluno.setRA(ra);
-              aluno.setCurso(curso);
+              
+//				Uso minimo de recursos - linhas
+//              aluno.setRA(ra);
+//              aluno.setCurso(curso);
 
-              if(alunos.size() == 0){
-                alunos.add(aluno);
+              
+//              Divisão do código em métodos - metodo validarAluno
+//              if(alunos.size() == 0){
+//                alunos.add(aluno);
+//              }else{
+//                n=0;
+//                for(i=0; i<alunos.size() ;i++){
+//                  if(aluno.getNome().equals(alunos.get(i).getNome()) ){
+//                    n=1;
+//                    System.out.println("Aluno ja cadastrado!");
+//                    break;
+//                  }
+                  
+                  // Uso minino de recursos - linhas
+//                  if(aluno.getRA().equals(alunos.get(i).getRA())){
+//                    n=1;
+//                    System.out.println("RA ja cadastrado!");
+//                    break;
+//                  }
+//                }
+//                if( n == 0 ){
+//                  alunos.add(aluno);
+//                }
+//              }
+
+              	// Uso Minimo de recursos
+//              i = alunos.size();
+//              System.out.println("Prosseguir para o proximo? <S/N>");
+              
+//              Nome de váriaveis compreensiveis
+//              sn = (char)System.in.read();
+              
+              opcaoContinuar = (char)System.in.read();
+              
+              //Variaveis com nome compreensiveis
+              //r.nextLine();
+              leitor.nextLine();
+              
+//              Nome de variaveis compreensiveis
+//              if(sn == 's' || sn == 'S'){
+              
+              if(opcaoContinuar == 's' || opcaoContinuar == 'S') {
+            	  opcaoMenu=1;
               }else{
-                n=0;
-                for(i=0; i<alunos.size() ;i++){
-                  if(aluno.getNome().equals(alunos.get(i).getNome()) ){
-                    n=1;
-                    System.out.println("Aluno ja cadastrado!");
-                    break;
-                  }
-                  if(aluno.getRA().equals(alunos.get(i).getRA())){
-                    n=1;
-                    System.out.println("RA ja cadastrado!");
-                    break;
-                  }
-                }
-                if( n == 0 ){
-                  alunos.add(aluno);
-                }
+            	  opcaoMenu=0;
               }
 
-              i = alunos.size();
-              System.out.println("Prosseguir para o proximo? <S/N>");
-              sn = (char)System.in.read();
-              r.nextLine();
-              if(sn == 's' || sn == 'S'){
-                x=1;
-              }else{
-                x=0;
-              }
-
-          }while(x==1);
+          }while(opcaoMenu==1);
             break;
 
           case 2:
-            i = alunos.size();
-            for(n=0; n<i ; n++){
-              aluno = alunos.get(n);
-              System.out.println("Alunos: "+aluno.getNome()+" "+aluno.getRA()+" "+aluno.getCurso());
-            }
+        	  
+//        	  modularizção do código - metodo listarAlunos
+//            i = alunos.size();
+//            for(n=0; n<i ; n++){
+//              aluno = alunos.get(n);
+//            System.out.println("Alunos: "+aluno.getNome()+" "+aluno.getRA()+" "+aluno.getCurso());
+//            }
             break;
 
           case 3:
@@ -99,5 +155,26 @@ public class App{
             System.out.println("Opção Inválida");
         }
       }while(a == 1);
+    }
+    
+    public static Boolean validarNome(String nome) {
+    	for(Aluno aluno : alunos) {
+    		if(aluno.getNome().equals(nome)) {
+    			System.out.println("Aluno ja cadastrado!");
+    			return false;
+    		} 
+    		return true;
+    	}
+    	return null;
+    }
+    
+    public static Boolean cadastrarAluno() {
+    	return null;
+    }
+    
+    public static void listarAluno() {
+    	for(Aluno aluno : alunos) {
+    		System.out.println("Aluno: "+aluno.getNome());
+    	}
     }
 }
